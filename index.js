aebb2f616937bc82a844a12748e12e4bdea8f63b
Sumit <Singh>
2019-08-07 16:54:45 +0530

    Initial Commit

---
+++ index.js
@@ -0,0 +1,132 @@
+const dialogflow = require('dialogflow');
+const uuid = require('uuid');
+const express = require('express');
+const cors = require('cors');
+const bodyParser  = require('body-parser');
+const jsforce = require('jsforce');
+const app = express();
+const port = process.env.port || 5000;
+
+const sessionId = uuid.v4,
+  //username ="pwrd-zg46@force.com",
+  username ="pwrdsumit@powerweave.com",
+  SecurityToken ="TszDHeS5nuvfDlli3I6RU7mY",
+  ConsumerKey = "3MVG9G9pzCUSkzZthsJ3o.DCpCyp3y5Ylr0Z9vr7MM8d.OQ7KfRGn6OkC2Ikcmm4b2PmtdyxbjgQBFUdVuytN",
+  ConsumerSecret = "ACE5D3C6427268B9FB8829F1E41CBECD70CB575E3BAC2CF7782D69B2F508F40A",
+  CallbackURL = "https://ap15.salesforce.com//oauth2/callback",
+  password = "power123TszDHeS5nuvfDlli3I6RU7mY";
+
+app.use(cors());
+//app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({
+    extended: false
+}));
+
+var conn = new jsforce.Connection({
+    // you can change loginUrl to connect to sandbox or prerelease env.
+    //Salesforce.com Enterprise Web Services API Version 46.0
+    oauth2 : {
+      // you can change loginUrl to connect to sandbox or prerelease env.
+      loginUrl : 'https://login.salesforce.com',
+      clientId : ConsumerKey,
+      clientSecret : ConsumerSecret,
+      redirectUri : CallbackURL
+    },
+    instanceUrl: 'https://ap15.salesforce.com',
+    accessToken: SecurityToken
+});
+
+
+app.get('/salesforceLogin', function (req, res) {  
+  conn.login(username, password, function(err, userInfo) {
+    if (err) { console.error(err);
+      return res.json({msg: `salesforceLogin Unsuccessfully!`,
+      username:username,password:password});
+    }
+    // Now you can get the access token and instance URL information.
+    // Save them to establish connection next time.
+    console.log('conn.accessToken',conn.accessToken);
+    console.log('conn.instanceUrl',conn.instanceUrl);
+    // logged in user property
+    console.log("User ID: " + userInfo.id);
+    console.log("Org ID: " + userInfo.organizationId);
+    res.json({msg: 'salesforceLogin Successfully!',accessToken:conn.accessToken
+    ,instanceUrl:conn.instanceUrl,User_ID:userInfo.id,Org_ID:userInfo.organizationId});
+  });
+//res.json({msg: 'salesforce Login  Failed!',username:username,password:password});
+});
+
+
+app.post('/send-msg',(req,res)=>{
+  console.log('req.body',req.body.input);
+  runSample(req.body.input).then(data =>{
+      res.send({'Reply': data});
+  })
+})
+
+app.post('/sendMsg',(req,res)=>{
+    console.log('req.body',req.body.MSG);
+    runSample(req.body.MSG).then(data =>{
+        res.send({'Reply': data});
+    })
+})
+
+app.post('/products/:id', function (req, res, next) {
+    res.json({msg: 'This is CORS-enabled for all origins!',
+    data: req.body
+})
+})
+
+app.post('/sf', function (req, res, next) {
+    res.json({data: req.body,next: next})
+})
+
+
+/**
+ * Send a query to the dialogflow agent, and return the query result.
+ * @param {string} projectId The project to be used
+ */
+async function runSample(msg,projectId = 'demolt-uhyhbh') {
+  // A unique identifier for the given session
+  const sessionId = uuid.v4();
+  debugger;
+
+  // Create a new session
+  const sessionClient = new dialogflow.SessionsClient({
+      keyFilename: "C:/Users/sumit.singh/Documents/Work/PoC/PixelOfImage/Works/Javascript/Node/New folder/tcsdemo/Server/demolt-6eaffb522ebe.json"
+  });
+  const sessionPath = sessionClient.sessionPath(projectId, sessionId);
+ 
+  // The text query request.
+  const request = {
+    session: sessionPath,
+    queryInput: {
+      text: {
+        // The query to send to the dialogflow agent
+        text: (msg)?msg:'hello',
+        // The language used by the client (en-US)
+        languageCode: 'en-US',
+      },
+    },
+  };
+ 
+  // Send request and log result
+  const responses = await sessionClient.detectIntent(request);
+  //console.log('Detected intent');
+  const result = responses[0].queryResult;
+  let responseMsg = result.fulfillmentText;
+  //console.log(`Query: ${result.queryText}`);
+  console.log(`Response: ${result.fulfillmentText}`);
+  if (result.intent.displayName === "Salesforce") {
+    responseMsg = "It Works";
+  }else if (result.intent) {
+    //console.log(`Intent: ${result.intent.displayName}`);
+  }  else {
+    //console.log(`No intent matched.`);
+  }
+  return responseMsg;
+}
+
+app.listen(port,()=>{
+    console.log(`running on port ${port}.`);
+})
